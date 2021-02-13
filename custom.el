7637b55dafec451044766267312726b45cc8877c
cezaris13 <pijus.cezar13@gmail.com>
2021-02-13 21:20:19 +0200

    initial commit

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/Desktop/empty/todo.org" "~/Desktop/empty/org-mode.org"))
+ '(package-selected-packages
+   '(treemacs-perspective treemacs-projectile nix-haskell-mode pager-default-keybindings smooth-scrolling))
+ '(send-mail-function 'mailclient-send-it))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
