22dbecadb8b09231b38797d3e9bf3b7ac1cd051a
cezaris13 <pijus.cezar13@gmail.com>
2021-03-12 19:19:42 +0200

    modified some snippets

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
    '("990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" "aaa4c36ce00e572784d424554dcc9641c82d1155370770e231e10c649b59a074" default))
  '(fancy-splash-image nil)
  '(org-agenda-files
-   '("~/Desktop/Org_mode/droutine.org" "~/Desktop/Org_mode/org-mode.org"))
+   '("~/Desktop/Org_mode/todo.org" "~/Desktop/Org_mode/droutine.org" "~/Desktop/Org_mode/org-mode.org"))
  '(org-agenda-log-mode-items '(closed clock state))
  '(org-agenda-time-grid
    '((daily today require-timed)
