2d2aaa2e137985ffd0bedad1fdc5e30228d41867
Pijus <pijus.petkevicius@bentley.com>
2024-12-20 17:44:39 +0200

    markdown mode, ein mode updates

--- custom.el
+++ custom.el
@@ -10,6 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ein:basecell-input-area-face ((t (:background "#363646"))))
  '(magit-blame-date ((t (:background "#363646" :weight bold))))
  '(magit-blame-hash ((t (:background "#363646" :weight bold))))
  '(magit-blame-heading ((t (:background "#363646" :weight bold))))
